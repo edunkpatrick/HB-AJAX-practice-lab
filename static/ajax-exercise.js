8ff7a409fcdb4f6b4ffc6d7fc2510d8403ed6f48
Erin Dunkley <eskilpatrick@gmail.com>
2023-01-11 13:03:03 -0800

    part 1 and part 2 complete

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,10 +3,15 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-  // TODO: get the fortune and show it in the #fortune-text div
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((status) => {
+      document.querySelector('#fortune-text').innerHTML = status;
+  })
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
+
 
 // PART 2: SHOW WEATHER
 
@@ -17,6 +22,13 @@ function showWeather(evt) {
   const zipcode = document.querySelector('#zipcode-field').value;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      document.querySelector('#weather-info').innerHTML = responseJson.forecast;
+    });
+
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
@@ -27,6 +39,36 @@ function orderMelons(evt) {
   evt.preventDefault();
 
   // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+  const formInputs = {
+    melon_type: document.querySelector('#melon-type-field').value,
+    qty: document.querySelector('#qty-field').value,
+  };
+  
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(formInputs),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      alert(responseJSON.msg);
+    });
+
 }
+
+  
+  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+// function showError(code) {
+
+//   if (code == "ERROR") {
+//     document.querySelector('#order-status').classList.add(order-error)';
+//     document.querySelector('#order-status').innerHTML = `${msg}`;
+//   }
+// }
+
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
+
